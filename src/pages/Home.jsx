6b526d33570142ef030847045c9caf67bb64ede4
Motasem Ali <motasemali0000@gmail.com>
2025-03-22 22:49:57 +0200

    add responsive our-services section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../ui/Hero";
 import WhyFlistia from "../ui/WhyFlistia";
+import OurServices from "../ui/OurServices";
 function Home() {
   return (
     <>
       <Hero />
       <WhyFlistia />
+      <OurServices />
     </>
   );
 }
