96acf3b2f6d80ff0aa33b40c86777d498b3c42f9
Motasem Ali <motasemali0000@gmail.com>
2025-03-22 09:51:59 +0200

    make the hero-section responsive

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -20,22 +20,22 @@ function Hero() {
       />
       <img src={MoonSlice} alt="" className="absolute bottom-0 w-full " />
       <Container maxWidth="lg">
-        <div className="hero-body flex flex-col justify-between items-center md:pt-20 pt-10 gap-10">
-          <div className="hero-content text-center">
-            <h1 className="title font-bold md:text-title text-4xl max-w-3xl md:leading-20 leading-12">
+        <div className="hero-body flex flex-col justify-between items-center pt-15 gap-10 md:pt-20 px-5">
+          <div className="hero-content text-center slide-in sm:w-1/2 lg:w-auto">
+            <h1 className="title font-bold text-5xl max-w-3xl leading-12 sm:leading-15 lg:leading-20 lg:text-6xl">
               Your Story Our Strategy{" "}
               <span className="text-heading-1 ">Your Success</span>
             </h1>
-            <p className="md:text-subtitle text-sm font-semibold text-paragraph max-w-lg mx-auto mt-8">
+            <p className="text-sm font-semibold text-paragraph max-w-lg mx-auto mt-8 lg:text-subtitle">
               Affordable, innovative marketing solutions tailored to small
               businesses and startups.
             </p>
           </div>
-          <div className="hero-cta flex flex-col justify-center items-center gap-10">
+          <div className="hero-cta flex flex-col justify-center items-center gap-10 z-1">
             <Link to="/contact-us">
               <Button>Contact Us</Button>
             </Link>
-            <IconButton className="drop-down m-auto w-fit">
+            <IconButton className="drop-down m-auto w-fit animate-bounce">
               <img className="w-6 h-6" src={dropDown} alt="" />
             </IconButton>
           </div>
