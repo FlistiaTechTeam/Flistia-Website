05c773c4090a8a409700ff0c5bf6261108138ed7
Motasem Ali <motasemali0000@gmail.com>
2025-05-16 23:57:21 +0300

    remove cache, fix logo fav icons, clear unwanted console logs

---
+++ src/ui/about-us/story.jsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material";
+function FlistiaStory() {
+  return (
+    <section
+      id="flistia-story"
+      className="flistia-story bg-separator relative py-8"
+    >
+      <Container maxWidth="lg">
+        <h2 className="section-title text-center">The Story of Flistia</h2>
+        <p className="text-center mb-5">Where Heritage Meets Innovation</p>
+      </Container>
+    </section>
+  );
+}
+
+export default FlistiaStory;
